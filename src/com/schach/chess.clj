768e4883aaaafea1ec286ad43ba444e68e32f087
prmioto <paulo.mioto@gmail.com>
2013-07-20 01:03:08 -0300

    Added possible moves for all the pieces.

--- src/com/schach/chess.clj
+++ src/com/schach/chess.clj
@@ -1,5 +1,7 @@
 (ns com.schach.chess)
 
+(defn abs [x] (if (>= x 0) x (* x -1)))
+
 (defn char-to-int [char-value base-char]
     (- (int char-value) (int base-char)))
 
@@ -10,7 +12,7 @@
         [(char-to-int rank-char rank-base-char) (char-to-int file-char file-base-char)]))
 
 (defn create-empty-square [color]
-    {:color color :piece-name nil :piece-color nil})
+    {:color color :piece nil})
     
 (defn create-rank [index width]
     (vec (take width 
@@ -28,37 +30,108 @@
 (def chess-piece-colors
     {\w :white \b :black})
 
+(defn create-piece [name color]
+    {:name name :color color :has-moved false})
+    
+(defn replace-piece [board coordinates new-piece]
+    (let [square (get-in board coordinates)]
+        (assoc-in board coordinates (assoc (dissoc square :piece) :piece new-piece))))
+
 (defn populate-board [board pieces]
     ;(println board pieces)
-    (reduce (fn [board piece]
-        ;(println "piece" piece board)
+    (reduce (fn [board piece-str]
+        ;(println "piece" piece-str)
 		(let [
-            piece-color-symbol (first piece)
-            piece-color (chess-piece-colors piece-color-symbol)
-            piece-name-symbol (first (rest piece))
-            piece-name (chess-piece-names piece-name-symbol)
-            position (rest (rest piece))
-            coordinates (from-algebric-to-coordinates position)
-            piece (get-in board coordinates)
-            new-piece (assoc 
-                (dissoc piece :piece-name :piece-color) :piece-color piece-color :piece-name piece-name)]
-            ;(println piece-color piece-name position coordinates piece new-piece)
-            (assoc-in board coordinates new-piece)))
+            color-symbol (first piece-str)
+            color (chess-piece-colors color-symbol)
+            name-symbol (first (rest piece-str))
+            name (chess-piece-names name-symbol)
+            algebric-position (rest (rest piece-str))
+            coordinates (from-algebric-to-coordinates algebric-position)
+            new-piece (create-piece name color)]
+            ;(println color name algebric-position coordinates new-piece)
+            (replace-piece board coordinates new-piece)))
             board pieces))
+
+(defn setup-chess-board []
+    (let [
+        chess-width 8 
+        board (create-board chess-width)]
+        (populate-board board 
+            ["wRa1" "wNb1" "wBc1" "wQd1" "wKe1" "wBf1" "wNg1" "wRh1"
+             "wPa2" "wPb2" "wPc2" "wPd2" "wPe2" "wPf2" "wPg2" "wPh2"
+             "bRa8" "bNb8" "bBc8" "bQd8" "bKe8" "bBf8" "bNg8" "bRh8"
+             "bPa7" "bPb7" "bPc7" "bPd7" "bPe7" "bPf7" "bPg7" "bPh7"])))
  
+(defn move-piece-by-coordinates [board from-coordinates to-coordinates]
+    (let [
+        from-square (get-in board from-coordinates)
+        to-square (get-in board to-coordinates)
+        piece (from-square :piece)]
+        (assoc-in (assoc-in board from-coordinates (dissoc from-square :piece)) to-coordinates (assoc to-square :piece piece))))
+        
+(defn move-piece [board from-algebric-position to-algebric-position]
+     (let [
+        from-coordinates (from-algebric-to-coordinates from-algebric-position)
+        to-coordinates (from-algebric-to-coordinates to-algebric-position)]
+     (move-piece-by-coordinates board from-coordinates to-coordinates)))
+
+(defmulti get-possible-moves (fn [piece coordinates width] (:name piece)))
+(defmethod get-possible-moves :king [piece coordinates width]
+    (let [[x y] coordinates]
+        (filter (fn [[x y]] (and (>= x 0) (>= y 0) (< x width) (< y width)))
+            (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)] 
+                [(+ dx x) (+ dy y)]))))
+(defmethod get-possible-moves :queen [piece coordinates width]
+    (let [[x y] coordinates]
+        (reduce concat
+            (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
+                (loop [x (+ dx x) y (+ dy y) coll []]
+                    (if (or (< x 0) (< y 0) (>= x width) (>= y width))
+                        coll
+                        (recur (+ dx x) (+ dy y) (conj coll [x y]))))))))                
+(defmethod get-possible-moves :bishop [piece coordinates width]
+    (let [[x y] coordinates]
+        (reduce concat
+            (for [dx [-1 0 1] dy [-1 0 1] :when (and (not= 0 dx) (not= 0 dy))]
+                (loop [x (+ dx x) y (+ dy y) coll []]
+                    (if (or (< x 0) (< y 0) (>= x width) (>= y width))
+                        coll
+                        (recur (+ dx x) (+ dy y) (conj coll [x y]))))))))    
+(defmethod get-possible-moves :rook [piece coordinates width]
+    (let [[x y] coordinates]
+        (reduce concat
+            (for [dx [-1 0 1] dy [-1 0 1] :when (and (not= 0 dx dy) (or (= 0 dx) (= 0 dy)))]
+                (loop [x (+ dx x) y (+ dy y) coll []]
+                    (if (or (< x 0) (< y 0) (>= x width) (>= y width))
+                        coll
+                        (recur (+ dx x) (+ dy y) (conj coll [x y]))))))))
+(defmethod get-possible-moves :pawn [piece coordinates width]
+    (let [[x y] coordinates]
+        (filter (fn [[x y]] (and (>= x 0) (>= y 0) (< x width) (< y width)))
+            (conj
+                (for [dx [-1 0 1]] 
+                    [(+ dx x) (+ 1 y)])
+                [x (+ 2 y)]))))
+(defmethod get-possible-moves :knight [piece coordinates width]
+    (let [[x y] coordinates]
+        (filter (fn [[x y]] (and (>= x 0) (>= y 0) (< x width) (< y width)))
+            (for [dx [-2 -1 1 2] dy [-2 -1 1 2] :when (not= (abs dx) (abs dy))]
+                    [(+ dx x) (+ dy y)]))))
+                
 (defn print-board [board]
     (println
         (reduce (fn [rank-str rank] 
             (str rank-str
                 (reduce (fn [square-str square] 
                     (let [
-                        square-color (square :color)
+                        square-color (square :colord)
                         is-dark (= square-color :dark)
-                        piece-name (square :piece-name)
-                        piece-color (square :piece-color)
+                        piece-name (-> square :piece :name)
+                        piece-color (-> square :piece :color)
                         is-empty (nil? piece-color)
                         is-black (= piece-color :black)]
-                        ;(println piece-name)
+                        ;(println (-> square :piece :name)
                         (str square-str 
                             (format "%s%s%3.3s%s%s" 
                                 (if (true? is-dark) "[" "(") 
--- test/com/schach/chess_test.clj
+++ test/com/schach/chess_test.clj
@@ -56,32 +56,119 @@
                     (is (= :light (:color (first odd-rank))))
                     (is (= :dark (:color (last odd-rank)))))))))
 
-                   
+(deftest test-replace-piece
+    (testing "Checks if a single piece is replaced in the board from a specified position."
+        (let [
+            chess-width 8 new-board (create-board chess-width)
+            coordinates [0 0]
+            old-piece {:name :king :color :black}
+            new-piece {:name :queen :color :white}
+            square (get-in new-board coordinates)
+            prepared-board (assoc-in new-board coordinates (assoc square :piece old-piece))]
+            (is (= new-piece (:piece (get-in (replace-piece prepared-board coordinates new-piece) coordinates)))))))
+
 (deftest test-populated-board
     (testing "Assures that the board is correctly populated."
         (let [chess-width 8 board (create-board chess-width)]
             (testing "Assigns a white king in the position a1."
                 (let [populated-board (populate-board board ["wKa1"])]
                 (print-board populated-board)
-                (is (= :king (-> (get-in populated-board [0 0]) :piece-name)))))
+                (is (= :king (-> (get-in populated-board [0 0]) :piece :name)))))
             (testing "Setup the white pieces."
                 (let [populated-board (populate-board board 
                     ["wRa1" "wNb1" "wBc1" "wQd1" "wKe1" "wBf1" "wNg1" "wRh1"
                      "wPa2" "wPb2" "wPc2" "wPd2" "wPe2" "wPf2" "wPg2" "wPh2"])]
                 (print-board populated-board)
-                (is (= :queen (-> (get-in populated-board [0 3]) :piece-name)))
-                (is (= :rook (-> (get-in populated-board [0 7]) :piece-name)))
-                (is (= :pawn (-> (get-in populated-board [1 7]) :piece-name)))))
-            (testing "Setup all the white and black pieces."
-                (let [populated-board (populate-board board 
-                    ["wRa1" "wNb1" "wBc1" "wQd1" "wKe1" "wBf1" "wNg1" "wRh1"
-                     "wPa2" "wPb2" "wPc2" "wPd2" "wPe2" "wPf2" "wPg2" "wPh2"
-                     "bRa8" "bNb8" "bBc8" "bQd8" "bKe8" "bBf8" "bNg8" "bRh8"
-                     "bPa7" "bPb7" "bPc7" "bPd7" "bPe7" "bPf7" "bPg7" "bPh7"])]
-                (print-board populated-board)
-                (is (= :queen (-> (get-in populated-board [0 3]) :piece-name)))
-                (is (= :rook (-> (get-in populated-board [0 7]) :piece-name)))
-                (is (= :pawn (-> (get-in populated-board [1 7]) :piece-name)))
-                (is (= :queen (-> (get-in populated-board [7 3]) :piece-name)))
-                (is (= :rook (-> (get-in populated-board [7 7]) :piece-name)))
-                (is (= :pawn (-> (get-in populated-board [6 7]) :piece-name))))))))
+                (is (= :queen (-> (get-in populated-board [0 3]) :piece :name)))
+                (is (= :rook (-> (get-in populated-board [0 7]) :piece :name)))
+                (is (= :pawn (-> (get-in populated-board [1 7]) :piece :name))))))))
+                
+(deftest test-setup-chess-board
+    (testing "Setup all the white and black pieces."
+        (let [chess-board (setup-chess-board)]
+        (print-board chess-board)
+        (is (= :queen (-> (get-in chess-board [0 3]) :piece :name)))
+        (is (= :rook (-> (get-in chess-board [0 7]) :piece :name)))
+        (is (= :pawn (-> (get-in chess-board [1 7]) :piece :name)))
+        (is (= :queen (-> (get-in chess-board [7 3]) :piece :name)))
+        (is (= :rook (-> (get-in chess-board [7 7]) :piece :name)))
+        (is (= :pawn (-> (get-in chess-board [6 7]) :piece :name))))))
+                
+(deftest test-move-piece
+    (testing "Checks if a piece is moved to the specified location."
+        (let [
+            board (setup-chess-board)
+            from-position "a1"
+            to-position "c4"
+            from-coordinates (from-algebric-to-coordinates from-position)
+            to-coordinates (from-algebric-to-coordinates to-position)
+            piece-to-move ((get-in board from-coordinates) :piece)
+            new-board (move-piece board from-position to-position)]
+        (print-board new-board)
+        (is (= piece-to-move ((get-in new-board to-coordinates) :piece))))))
+        
+(deftest test-possible-moves
+    (testing "Checks the possible moves for the king from a given coordinates."
+        (let [king (create-piece :king :white)]
+            (testing "Checks for possible moves for a king in the left-bottom corner."
+                (is (= '([0 1] [1 0] [1 1]) (get-possible-moves king [0 0] 8))))
+            (testing "Checks for possible moves for a king in the board center."
+                (is (= '([3 3] [3 4] [3 5] [4 3] [4 5] [5 3] [5 4] [5 5]) (get-possible-moves king [4 4] 8))))))
+    (testing "Checks the possible moves for the queen from a given coordinates."
+        (let [queen (create-piece :queen :white)]
+            (testing "Checks for possible moves for a queen in the left-bottom corner."
+                (is (= '(
+                        [0 1] [0 2] [0 3] [0 4] [0 5] [0 6] [0 7] ; dx = 0 and dy = 1
+                        [1 0] [2 0] [3 0] [4 0] [5 0] [6 0] [7 0] ; dx = 1 and dy = 0
+                        [1 1] [2 2] [3 3] [4 4] [5 5] [6 6] [7 7] ; dx = 1 and dy = 1
+                    ) (get-possible-moves queen [0 0] 8))))
+            (testing "Checks for possible moves for a queen in the board center."
+                (is (= '(
+                        [3 3] [2 2] [1 1] [0 0] ; dx = -1 and dy = -1
+                        [3 4] [2 4] [1 4] [0 4] ; dx = -1 and dy = 0
+                        [3 5] [2 6] [1 7] ; dx = -1 and dy = 1
+                        [4 3] [4 2] [4 1] [4 0] ; dx = 0 and dy = -1
+                        [4 5] [4 6] [4 7] ; dx = 0 and dy = 1
+                        [5 3] [6 2] [7 1] ; dx = 1 and dy = -1
+                        [5 4] [6 4] [7 4] ; dx = 1 and dy = 0
+                        [5 5] [6 6] [7 7] ; dx = 1 and dy = 1
+                    ) (get-possible-moves queen [4 4] 8))))))
+    (testing "Checks the possible moves for the bishop from a given coordinates."
+        (let [bishop (create-piece :bishop :white)]
+            (testing "Checks for possible moves for a bishop in the left-bottom corner."
+                (is (= '(
+                        [1 1] [2 2] [3 3] [4 4] [5 5] [6 6] [7 7] ; dx = 1 and dy = 1
+                    ) (get-possible-moves bishop [0 0] 8))))
+            (testing "Checks for possible moves for a bishop in the board center."
+                (is (= '(
+                        [3 3] [2 2] [1 1] [0 0] ; dx = -1 and dy = -1
+                        [3 5] [2 6] [1 7] ; dx = -1 and dy = 1
+                        [5 3] [6 2] [7 1] ; dx = 1 and dy = -1
+                        [5 5] [6 6] [7 7] ; dx = 1 and dy = 1
+                    ) (get-possible-moves bishop [4 4] 8))))))
+    (testing "Checks the possible moves for the rook from a given coordinates."
+        (let [rook (create-piece :rook :white)]
+            (testing "Checks for possible moves for a rook in the left-bottom corner."
+                (is (= '(
+                        [0 1] [0 2] [0 3] [0 4] [0 5] [0 6] [0 7] ; dx = 0 and dy = 1
+                        [1 0] [2 0] [3 0] [4 0] [5 0] [6 0] [7 0] ; dx = 1 and dy = 0
+                    ) (get-possible-moves rook [0 0] 8))))
+            (testing "Checks for possible moves for a rook in the board center."
+                (is (= '(
+                        [3 4] [2 4] [1 4] [0 4] ; dx = -1 and dy = 0
+                        [4 3] [4 2] [4 1] [4 0] ; dx = 0 and dy = -1
+                        [4 5] [4 6] [4 7] ; dx = 0 and dy = 1
+                        [5 4] [6 4] [7 4] ; dx = 1 and dy = 0
+                    ) (get-possible-moves rook [4 4] 8))))))
+    (testing "Checks the possible moves for the pawn from a given coordinates."
+        (let [pawn (create-piece :pawn :white)]
+            (testing "Checks for possible moves for a pawn in the left-bottom corner."
+                (is (= '([0 2] [0 1] [1 1]) (get-possible-moves pawn [0 0] 8))))
+            (testing "Checks for possible moves for a pawn in the board center."
+                (is (= '([4 6] [3 5] [4 5] [5 5]) (get-possible-moves pawn [4 4] 8))))))
+    (testing "Checks the possible moves for the knight from a given coordinates."
+        (let [knight (create-piece :knight :white)]
+            (testing "Checks for possible moves for a knight in the left-bottom corner."
+                (is (= '([1 2] [2 1]) (get-possible-moves knight [0 0] 8))))
+            (testing "Checks for possible moves for a knight in the board center."
+                (is (= '([2 3] [2 5] [3 2] [3 6] [5 2] [5 6] [6 3] [6 5]) (get-possible-moves knight [4 4] 8)))))))
